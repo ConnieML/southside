3c016b41913e3f52fb63ef86f47f5880911cff6a
chris <chris@chriss-MacBook-Pro.local>
2023-07-06 14:25:03 -0400

    more appConfigs

--- public/assets/appConfig.js
+++ public/assets/appConfig.js
@@ -5,26 +5,26 @@ var accountSid = "AC595d7affd2fb2cdb37a528cb25e5d63f";
 var appConfig = {
   // Uncomment the following lines to enable automatic log innn
   
-  sso: {
-    accountSid: AC595d7affd2fb2cdb37a528cb25e5d63f
-  }
-  sdkOptions: {
-    worker: {
-      logLevel: "error"
-    },
-    insights: {
-      logLevel: "error"
-    },
-    chat: {
-      logLevel: "error"
-    },
-    flex: {
-      logger: {
-        level: "error"
-      }
-    },
-    voice: {
-      debug: false
-    }
-  }
+  // sso: {
+  //   accountSid: AC595d7affd2fb2cdb37a528cb25e5d63f
+  // }
+  // sdkOptions: {
+  //   worker: {
+  //     logLevel: "error"
+  //   },
+  //   insights: {
+  //     logLevel: "error"
+  //   },
+  //   chat: {
+  //     logLevel: "error"
+  //   },
+  //   flex: {
+  //     logger: {
+  //       level: "error"
+  //     }
+  //   },
+  //   voice: {
+  //     debug: false
+  //   }
+  // }
 }
