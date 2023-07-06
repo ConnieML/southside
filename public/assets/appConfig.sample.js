9f1b584df9e9d0dae80358a5113a61604b1a8c53
chris <chris@chriss-MacBook-Pro.local>
2023-07-05 20:50:21 -0400

    adding react files

---
+++ public/assets/appConfig.sample.js
@@ -0,0 +1,30 @@
+// set your account sid here
+var accountSid = "AC595d7affd2fb2cdb37a528cb25e5d63f";
+
+
+var appConfig = {
+  // Uncomment the following lines to enable automatic log in
+  
+  sso: {
+    accountSid: AC595d7affd2fb2cdb37a528cb25e5d63f
+  }
+  sdkOptions: {
+    worker: {
+      logLevel: "error"
+    },
+    insights: {
+      logLevel: "error"
+    },
+    chat: {
+      logLevel: "error"
+    },
+    flex: {
+      logger: {
+        level: "error"
+      }
+    },
+    voice: {
+      debug: false
+    }
+  }
+}
